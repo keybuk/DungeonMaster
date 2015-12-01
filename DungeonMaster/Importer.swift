63c6aa7a3068e61fed126a1e0699cc3e4e9bb594
Scott James Remnant <scott@netsplit.com>
2015-11-30 23:22:42 -0800

    Finish out the text-based model and importer
    Verified the resulting sqlite and it looks good!

--- DungeonMaster/Importer.swift
+++ DungeonMaster/Importer.swift
@@ -30,7 +30,12 @@ public func importIfNeeded(managedObjectContext: NSManagedObjectContext) {
 enum NextLine {
     case Name
     case Source
-    case WaitingForEnd
+    case SizeTypeAlignment
+    case EndOfIntro
+    case BasicStats
+    case AbilityScores
+    case AdvancedStats
+    case Text
 }
 
 func importIntoContext(managedObjectContext: NSManagedObjectContext) {
@@ -78,12 +83,105 @@ func importIntoContext(managedObjectContext: NSManagedObjectContext) {
                     source.section = sourceTextParts[1]
                 }
             }
-            nextLine = .WaitingForEnd
-        case .WaitingForEnd:
+            nextLine = .SizeTypeAlignment
+        case .SizeTypeAlignment:
+            monster!.sizeTypeAlignment = line
+            nextLine = .EndOfIntro
+        case .EndOfIntro:
+            if line != "" {
+                print("Unexpected line!")
+                abort()
+            }
+            nextLine = .BasicStats
+        case .BasicStats:
+            let acTag = "Armor Class "
+            let hpTag = "Hit Points "
+            let sTag = "Speed "
+            if line == "" {
+                nextLine = .AbilityScores
+            } else if line.hasPrefix(acTag) {
+                monster!.armorClass = line.substringFromIndex(line.startIndex.advancedBy(acTag.characters.count))
+            } else if line.hasPrefix(hpTag) {
+                monster!.hitPoints = line.substringFromIndex(line.startIndex.advancedBy(hpTag.characters.count))
+            } else if line.hasPrefix(sTag) {
+                monster!.speed = line.substringFromIndex(line.startIndex.advancedBy(sTag.characters.count))
+            } else {
+                print("Bad basic stats line: \(line)")
+                abort()
+            }
+        case .AbilityScores:
+            if line == "" {
+                nextLine = .AdvancedStats
+            } else if line.hasPrefix("STR ") {
+                monster!.strength = line.substringFromIndex(line.startIndex.advancedBy(4))
+            } else if line.hasPrefix("DEX ") {
+                monster!.dexterity = line.substringFromIndex(line.startIndex.advancedBy(4))
+            } else if line.hasPrefix("CON ") {
+                monster!.constitution = line.substringFromIndex(line.startIndex.advancedBy(4))
+            } else if line.hasPrefix("INT ") {
+                monster!.intelligence = line.substringFromIndex(line.startIndex.advancedBy(4))
+            } else if line.hasPrefix("WIS ") {
+                monster!.wisdom = line.substringFromIndex(line.startIndex.advancedBy(4))
+            } else if line.hasPrefix("CHA ") {
+                monster!.charisma = line.substringFromIndex(line.startIndex.advancedBy(4))
+            } else {
+                print("Bad ability scores line: \(line)")
+                abort()
+            }
+        case .AdvancedStats:
+            let stTag = "Saving Throws "
+            let skTag = "Skills "
+            let dvTag = "Damage Vulnerabilities "
+            let drTag = "Damage Resistances "
+            let drTag2 = "Damage Resistance "
+            let diTag = "Damage Immunities "
+            let ciTag = "Condition Immunities "
+            let seTag = "Senses "
+            let lTag = "Languages "
+            let cTag = "Challenge "
+            if line == "" {
+                nextLine = .Text
+            } else if line.hasPrefix(stTag) {
+                monster!.savingThrows = line.substringFromIndex(line.startIndex.advancedBy(stTag.characters.count))
+            } else if line.hasPrefix(skTag) {
+                monster!.skills = line.substringFromIndex(line.startIndex.advancedBy(skTag.characters.count))
+            } else if line.hasPrefix(seTag) {
+                monster!.senses = line.substringFromIndex(line.startIndex.advancedBy(seTag.characters.count))
+            } else if line.hasPrefix(dvTag) {
+                monster!.damageVulnerabilities = line.substringFromIndex(line.startIndex.advancedBy(dvTag.characters.count))
+            } else if line.hasPrefix(drTag) {
+                monster!.damageResistances = line.substringFromIndex(line.startIndex.advancedBy(drTag.characters.count))
+            } else if line.hasPrefix(drTag2) {
+                monster!.damageResistances = line.substringFromIndex(line.startIndex.advancedBy(drTag2.characters.count))
+            } else if line.hasPrefix(diTag) {
+                monster!.damageImmunities = line.substringFromIndex(line.startIndex.advancedBy(diTag.characters.count))
+            } else if line.hasPrefix(ciTag) {
+                monster!.conditionImmunities = line.substringFromIndex(line.startIndex.advancedBy(ciTag.characters.count))
+            } else if line.hasPrefix(lTag) {
+                monster!.languages = line.substringFromIndex(line.startIndex.advancedBy(lTag.characters.count))
+            } else if line.hasPrefix(cTag) {
+                monster!.challenge = line.substringFromIndex(line.startIndex.advancedBy(cTag.characters.count))
+            } else {
+                print("Bad advanced stats line: \(line)")
+                abort()
+            }
+        case .Text:
             if line == "--" {
                 nextLine = .Name
+            } else {
+                monster!.text += line + "\n"
             }
         }
     }
-    
+
+    do {
+        try managedObjectContext.save()
+    } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nserror = error as NSError
+        NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+        abort()
+    }
+
 }
--- DungeonMaster/Monster.swift
+++ DungeonMaster/Monster.swift
@@ -12,6 +12,26 @@ import CoreData
 public final class Monster: NSManagedObject {
     
     @NSManaged var name: String
+    @NSManaged var sizeTypeAlignment: String
+    @NSManaged var armorClass: String
+    @NSManaged var hitPoints: String
+    @NSManaged var speed: String
+    @NSManaged var strength: String
+    @NSManaged var dexterity: String
+    @NSManaged var constitution: String
+    @NSManaged var intelligence: String
+    @NSManaged var wisdom: String
+    @NSManaged var charisma: String
+    @NSManaged var savingThrows: String?
+    @NSManaged var skills: String?
+    @NSManaged var damageVulnerabilities: String?
+    @NSManaged var damageResistances: String?
+    @NSManaged var damageImmunities: String?
+    @NSManaged var conditionImmunities: String?
+    @NSManaged var senses: String
+    @NSManaged var languages: String
+    @NSManaged var challenge: String
+    @NSManaged var text: String
     @NSManaged var sources: NSSet
     
     public convenience init(name: String, inManagedObjectContext context: NSManagedObjectContext) {
