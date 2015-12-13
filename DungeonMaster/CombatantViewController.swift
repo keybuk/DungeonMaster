62ba2b30b4720352019d63d391782589fedcdc98
Scott James Remnant <scott@netsplit.com>
2015-12-13 14:40:30 -0800

    Combatant: remove - in mark

--- DungeonMaster/CombatantViewController.swift
+++ DungeonMaster/CombatantViewController.swift
@@ -46,7 +46,7 @@ class CombatantViewController: UITableViewController {
     }
 
     /*
-    // MARK: - Navigation
+    // MARK: Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
