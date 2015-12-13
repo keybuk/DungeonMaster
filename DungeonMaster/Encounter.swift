96df6c29778afff640702b279fb8e65c29be7b78
Scott James Remnant <scott@netsplit.com>
2015-12-13 14:14:27 -0800

    Encounter: update lastUsed on changes
    Use automatic updating rather than manual as an experiment

--- DungeonMaster/Encounter.swift
+++ DungeonMaster/Encounter.swift
@@ -16,11 +16,48 @@ final class Encounter: NSManagedObject {
     
     @NSManaged var combatants: NSSet
     
+    var notificationObserver: NSObjectProtocol?
+    
     convenience init(inManagedObjectContext context: NSManagedObjectContext) {
         let entity = NSEntityDescription.entity(Model.Encounter, inManagedObjectContext: context)
         self.init(entity: entity, insertIntoManagedObjectContext: context)
         
         lastUsed = NSDate()
+    }
+    
+    deinit {
+        if let notificationObserver = notificationObserver {
+            NSNotificationCenter.defaultCenter().removeObserver(notificationObserver)
+        }
+    }
+
+    override func awakeFromInsert() {
+        super.awakeFromInsert()
+        
+        observeChanges()
+    }
+    
+    override func awakeFromFetch() {
+        super.awakeFromFetch()
+        
+        observeChanges()
+    }
+    
+    /// Encounter maintains a lastUsed property that is updated automatically when the encounter object itself changes, combatants are added to or removed from the encounter, or any of the combatants in the encounter is changed.
+    func observeChanges() {
+        guard notificationObserver == nil else { return }
+    
+        notificationObserver = NSNotificationCenter.defaultCenter().addObserverForName(NSManagedObjectContextObjectsDidChangeNotification, object: managedObjectContext, queue: nil) { notification in
+            if let updatedObjects = notification.userInfo?[NSUpdatedObjectsKey] as? NSSet {
+                var interestingObjects = Set<NSManagedObject>()
+                interestingObjects.insert(self)
+                interestingObjects.unionInPlace(self.combatants.allObjects as! [NSManagedObject])
+                
+                if updatedObjects.intersectsSet(interestingObjects) {
+                    self.setPrimitiveValue(NSDate(), forKey: "lastUsed")
+                }
+            }
+        }
     }
     
 }
