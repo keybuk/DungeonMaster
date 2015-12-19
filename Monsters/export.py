f88eddc9c1465b6fb153ba8cf0f4b8fe4d0cfeee
Scott James Remnant <scott@netsplit.com>
2015-12-19 00:47:16 -0800

    export: check XP against CR table
    Do this here since we never place the XP in the output plist, so it's lost data if there isn't a match.

--- Monsters/export.py
+++ Monsters/export.py
@@ -63,6 +63,43 @@ SPELL_RE = re.compile(r'/([a-z ]+)/')
 
 CHALLENGE_RE = re.compile(r'^([0-9/]+) \(([0-9,]+)(?: XP)?\)$')
 
+XP = {
+	"0": "10",
+	"1/8": "25",
+	"1/4": "50",
+	"1/2": "100",
+	"1": "200",
+	"2": "450",
+	"3": "700",
+	"4": "1,100",
+	"5": "1,800",
+	"6": "2,300",
+	"7": "2,900",
+	"8": "3,900",
+	"9": "5,000",
+	"10": "5,900",
+	"11": "7,200",
+	"12": "8,400",
+	"13": "10,000",
+	"14": "11,500",
+	"15": "13,000",
+	"16": "15,000",
+	"17": "18,000",
+	"18": "20,000",
+	"19": "22,000",
+	"20": "25,000",
+	"21": "33,000",
+	"22": "41,000",
+	"23": "50,000",
+	"24": "62,000",
+	"25": "75,000",
+	"26": "90,000",
+	"27": "105,000",
+	"28": "120,000",
+	"29": "135,000",
+	"30": "155,000",
+}
+
 class Exporter(monster.MonsterParser):
 
 	def __init__(self, filename, bookTags):
@@ -335,6 +372,10 @@ class Exporter(monster.MonsterParser):
 			raise self.error("Challenge didn't match expected format: %s" % line)
 
 		(cr, xp) = match.groups()
+		if XP[cr] != xp:
+			if cr != "0" or xp != "0":
+				raise self.error("XP didn't match expected for challenge: %s" % xp)
+
 		if cr == '1/8':
 			cr = 1.0/8
 		elif cr == '1/4':
