c13f201a71ab07143bc02931dbab467963b407b9
Scott James Remnant <scott@netsplit.com>
2016-01-18 09:44:31 -0800

    Encounter: get rid of lastUsed shenanigans
    These would only work if the Encounter object wasn't faulted anyway, and that was never guaranteed. Just prefer explicit updating wherever we end up doing that.

--- DungeonMaster/Encounter.swift
+++ DungeonMaster/Encounter.swift
@@ -46,50 +46,13 @@ final class Encounter: NSManagedObject {
 
     // MARK: Internal properties
     
-    var notificationObserver: NSObjectProtocol?
-    
     convenience init(inManagedObjectContext context: NSManagedObjectContext) {
         let entity = NSEntityDescription.entity(Model.Encounter, inManagedObjectContext: context)
         self.init(entity: entity, insertIntoManagedObjectContext: context)
         
         lastUsed = NSDate()
     }
-    
-    deinit {
-        if let notificationObserver = notificationObserver {
-            NSNotificationCenter.defaultCenter().removeObserver(notificationObserver)
-        }
-    }
 
-    override func awakeFromInsert() {
-        super.awakeFromInsert()
-        
-        observeChanges()
-    }
-    
-    override func awakeFromFetch() {
-        super.awakeFromFetch()
-        
-        observeChanges()
-    }
-    
-    /// Encounter maintains a lastUsed property that is updated automatically when the encounter object itself changes, combatants are added to or removed from the encounter, or any of the combatants in the encounter is changed.
-    func observeChanges() {
-        guard notificationObserver == nil else { return }
-    
-        notificationObserver = NSNotificationCenter.defaultCenter().addObserverForName(NSManagedObjectContextObjectsDidChangeNotification, object: managedObjectContext, queue: nil) { notification in
-            if let updatedObjects = notification.userInfo?[NSUpdatedObjectsKey] as? NSSet {
-                var interestingObjects = Set<NSManagedObject>()
-                interestingObjects.insert(self)
-                interestingObjects.unionInPlace(self.combatants.allObjects as! [NSManagedObject])
-                
-                if updatedObjects.intersectsSet(interestingObjects) {
-                    self.setPrimitiveValue(NSDate(), forKey: "lastUsed")
-                }
-            }
-        }
-    }
-    
     /// Returns the total XP for all monsters in the encounter.
     func totalXP() -> Int {
         return combatants.filter({ ($0 as! Combatant).role == .Foe }).map({ ($0 as! Combatant).monster!.XP }).reduce(0, combine: +)
