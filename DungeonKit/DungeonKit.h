4514ccf33742bdf95549087e269f3eaa2179c6af
Scott James Remnant <scott@netsplit.com>
2016-02-07 12:11:53 -0800

    DungeonKit: add framework to share between both apps
    Adding as a linked framework means that it's a build dependency, which means I don't have to build the framework by hand for every update.

---
+++ DungeonKit/DungeonKit.h
@@ -0,0 +1,19 @@
+//
+//  DungeonKit.h
+//  DungeonKit
+//
+//  Created by Scott James Remnant on 2/7/16.
+//  Copyright © 2016 Scott James Remnant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DungeonKit.
+FOUNDATION_EXPORT double DungeonKitVersionNumber;
+
+//! Project version string for DungeonKit.
+FOUNDATION_EXPORT const unsigned char DungeonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DungeonKit/PublicHeader.h>
+
+
